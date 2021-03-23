9cc653e951fd97345c78c3e75d1f78a2ae580df3
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-23 12:30:56 +0000

    fixed variable

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // setup standard variables
-const d = 0
+let d = 0
 let r = 0
 let a = 0
 let c = 0
@@ -9,7 +9,7 @@ document.getElementById('button').addEventListener('click', checker)
 // functions
 // checks if the input is above or below 0 if its below it will show an error mesage if its above it will trigger the calculate function
 function checker () {
-  let d = document.getElementById('input').value
+  d = document.getElementById('input').value
   d = parseInt(d)
   if (d < 0) {
     alert('error:the inputed diameter must be above 0')
@@ -20,7 +20,7 @@ function checker () {
 }
 // calculates the Area, Circomference and Radius and displays them
 function calculate () {
-  let d = document.getElementById('input').value
+  d = document.getElementById('input').value
   d = parseInt(d)
   r = d / 2
   document.getElementById('out3').innerHTML = r
